afd2edd4c561ccde4e5d77c9ad37e1dfa4f472f7
Doutsy <julien.sebastien.herr@gmail.com>
2021-12-01 17:10:28 +0100

    Commit Day01

---
+++ Day01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2021
+{
+    class Day01
+    {
+        public static void Exec(string _path, int _part)
+        {
+            string[] lines = File.ReadAllText(_path).Split("\n", StringSplitOptions.TrimEntries);
+            int previousDepth = int.MaxValue;
+            int currentDepth = 0;
+            int answer = 0;
+
+            switch (_part)
+            {
+                case 1:
+                    foreach (string line in lines)
+                    {
+                        currentDepth = int.Parse(line);
+                        if (currentDepth > previousDepth)
+                        {
+                            answer++;
+                        }
+                        previousDepth = currentDepth;
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < lines.Length - 2; i++)
+                    {
+                        currentDepth = int.Parse(lines[i]) + int.Parse(lines[i + 1]) + int.Parse(lines[i + 2]);
+                        if (currentDepth > previousDepth)
+                        {
+                            answer++;
+                        }
+                        previousDepth = currentDepth;
+                    }
+                    break;
+            }
+            Console.WriteLine($"Day 01 | Part {_part} : {answer}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace AOC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day01.Exec(@"C:\Users\jherr\Desktop\AdventOfCode2021\AOC2021\AOC2021\Inputs\01.txt", 1);
+            Day01.Exec(@"C:\Users\jherr\Desktop\AdventOfCode2021\AOC2021\AOC2021\Inputs\01.txt", 2);
+        }
+    }
+}
